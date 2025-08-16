e43ff1a864774c293dbd1ed56d0aca04f42baf3a
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-08-16 22:29:14 +0900

    chore/コメントの追加

--- internal/interface/repository/user_repository.go
+++ internal/interface/repository/user_repository.go
@@ -4,8 +4,18 @@ import (
 	"todo_backend/internal/domain"
 )
 
+// UserRepositoryはユーザエンティティの永続化をを抽象化したインターフェースです。
+// DBの種類や実装に依存せず、ユースケース側から利用されます。
 type UserRepository interface {
+	// Createは新しいユーザーを永続化します。
+	// すでに同じEmailが存在する場合はエラーを返します。
 	Create(user *domain.User) error
+
+	// FindByEmailは指定したEmailに一致するユーザーを取得します。
+	// ユーザーが存在しない場合はエラーを返します。
 	FindByEmail(email string) (*domain.User, error)
+
+	// FindByIDは指定したIDに一致するユーザーを取得します。
+	// ユーザーが存在しない場合はエラーを返します。
 	FindByID(id uint) (*domain.User, error)
 }
