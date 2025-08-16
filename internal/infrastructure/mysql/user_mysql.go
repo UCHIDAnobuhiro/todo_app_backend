2b9280db4c3119e07c3c6216c5548016b7594e91
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-08-16 22:17:33 +0900

    chore/コメントの追加

--- internal/infrastructure/mysql/user_mysql.go
+++ internal/infrastructure/mysql/user_mysql.go
@@ -7,18 +7,25 @@ import (
 	"gorm.io/gorm"
 )
 
+// userMySQLはUserRepositoryインターフェースのMySQLの実装です。
+// GORMを使用してデータベースの操作を行います。
 type userMySQL struct {
 	db *gorm.DB
 }
 
+// NewUserMySQLはuserMySQLの新しいインスタンスを返します。
+// 引数dbは GORMの*gorm.DBで、DB接続済みのオブジェクトを渡してください。
 func NewUserMySQL(db *gorm.DB) repository.UserRepository {
 	return &userMySQL{db: db}
 }
 
+// CreateはユーザをDBに追加します。
 func (r *userMySQL) Create(u *domain.User) error {
 	return r.db.Create(u).Error
 }
 
+// FindByEmailはEmailをキーにユーザを検索します。
+// 該当するユーザが存在しない場合はエラーを返します。
 func (r *userMySQL) FindByEmail(email string) (*domain.User, error) {
 	var u domain.User
 	if err := r.db.Where("email = ?", email).First(&u).Error; err != nil {
@@ -27,6 +34,8 @@ func (r *userMySQL) FindByEmail(email string) (*domain.User, error) {
 	return &u, nil
 }
 
+// FindByIDはIDをキーにユーザを検索します。
+// 該当するユーザが存在しない場合、エラーを返します。
 func (r *userMySQL) FindByID(id uint) (*domain.User, error) {
 	var u domain.User
 	if err := r.db.First(&u, id).Error; err != nil {
